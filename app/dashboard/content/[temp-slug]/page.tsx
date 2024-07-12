dd6505da7b02314d1f52a008df54bd77c6d72879
Nanthan S Nair <143941884+Nanthan-s-Nair@users.noreply.github.com>
2024-07-12 23:13:48 +0530

    done dusted--

--- app/dashboard/content/[temp-slug]/page.tsx
+++ app/dashboard/content/[temp-slug]/page.tsx
@@ -16,38 +16,39 @@ interface PROPS {
     }
 }
 
-function cc(props: PROPS) {
-    const reqtemp : TEMPLATE|undefined=Template?.find((item)=>item.slug==props.params['temp-slug']);
-    const[loading,setloading]=useState(false);
-    const [aiout,setaiout]=useState<string>('');
-    const {user}=useUser();
-    const Aicontent=async(fdata:any)=>{
-        setloading(true);
-        const sele=reqtemp?.prompt;
-        const finalprom=JSON.stringify(fdata)+","+sele;
-        const result=await chatSession.sendMessage(finalprom);
+const Cc: React.FC<PROPS> = (props) => {
+    const reqtemp: TEMPLATE | undefined = Template?.find((item) => item.slug === props.params['temp-slug']);
+    const [loading, setLoading] = useState(false);
+    const [aiout, setAiout] = useState<string>('');
+    const { user } = useUser();
+
+    const Aicontent = async (fdata: any) => {
+        setLoading(true);
+        const sele = reqtemp?.prompt;
+        const finalprom = JSON.stringify(fdata) + "," + sele;
+        const result = await chatSession.sendMessage(finalprom);
         console.log(result.response.text());
-        setaiout(result?.response.text());
-        await savedb(fdata,reqtemp?.slug,result?.response.text());
-        setloading(false);
+        setAiout(result?.response.text());
+        await savedb(fdata, reqtemp?.slug, result?.response.text());
+        setLoading(false);
     }
 
-    const savedb = async(fdata:any,slug:any,aiout:string)=>{
-        const result=await db.insert(AIOutput).values({
-            formData:fdata,
-            templateSlug:slug,
-            aiResponse:aiout,
-            createdBy:user?.primaryEmailAddress?.emailAddress,
-            createdAt:moment().format('DD/MM/yyyy'),
+    const savedb = async (fdata: any, slug: any, aiout: string) => {
+        const result = await db.insert(AIOutput).values({
+            formData: fdata,
+            templateSlug: slug,
+            aiResponse: aiout,
+            createdBy: user?.primaryEmailAddress?.emailAddress,
+            createdAt: moment().format('DD/MM/yyyy'),
         })
     }
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5 bg-slate-100'>
-            <Inputsec reqtemp={reqtemp} userinp={(r:any)=>Aicontent(r)} loading={loading}/>
+            <Inputsec reqtemp={reqtemp} userinp={(r: any) => Aicontent(r)} loading={loading}/>
             <div className='col-span-2'><Outputsec aiout={aiout}/></div>
         </div>
     )
 }
 
-export default cc
+export default Cc;
